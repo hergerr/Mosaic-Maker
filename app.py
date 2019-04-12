cf34f663d6a80aed19337a68fbddf6bbfdd1d0d1
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-04-12 15:32:57 +0200

    Make displaying pics from url

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/mozaika')
 def mosaic():
+    # default values of requst arguments
+    x_resolution = 2048
+    y_resolution = 2048
+    random = False
+
     request_arguments = request.args.to_dict(flat=False)
-    return request_arguments.__repr__()
+    # check if optional argument (random) exists
+    if 'losowo' in request_arguments and request_arguments['losowo'][0] == '1':
+        random = True
+
+    # check if option argument (resolution) exists
+    if 'rozdzielczosc' in request_arguments:
+        # splitting x and y resolution values by 'x' and mapping it on int
+        x_resolution, y_resolution = map(int, request_arguments['rozdzielczosc'][0].split('x'))
+
+    if 'zdjecia' in request_arguments:
+        pic_list = request_arguments['zdjecia'][0].split(',')
+
+    print(pic_list)
+    return render_template('mosaic.html', pic_list=pic_list)
 
 
 @app.route('/')

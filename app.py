62768834230d2560ffa34174da9134a98e3b5937
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-04-12 18:33:35 +0200

    Make image resizing

--- app.py
+++ app.py
@@ -1,35 +1,59 @@
 from flask import Flask, request, redirect, url_for, render_template
+from PIL import Image
+import requests
+from io import BytesIO
 
 app = Flask(__name__)
 
 
 @app.route('/mozaika')
 def mosaic():
-    # default values of requst arguments
+    """This function deals with given url and returns template with mosaic"""
+
+    # list with url for pictures
+    pic_url_list = []
+
+    # default resolution
     x_resolution = 2048
     y_resolution = 2048
-    random = False
 
     request_arguments = request.args.to_dict(flat=False)
-    # check if optional argument (random) exists
-    if 'losowo' in request_arguments and request_arguments['losowo'][0] == '1':
-        random = True
-
     # check if option argument (resolution) exists
     if 'rozdzielczosc' in request_arguments:
         # splitting x and y resolution values by 'x' and mapping it on int
         x_resolution, y_resolution = map(int, request_arguments['rozdzielczosc'][0].split('x'))
 
     if 'zdjecia' in request_arguments:
-        pic_list = request_arguments['zdjecia'][0].split(',')
+        pic_url_list = request_arguments['zdjecia'][0].split(',')
 
-    print(pic_list)
-    return render_template('mosaic.html', pic_list=pic_list)
+    # check if optional argument (random) exists
+    if 'losowo' in request_arguments and request_arguments['losowo'][0] == '1':
+        map(set, pic_url_list)
+
+    modify_images(pic_url_list, x_resolution, y_resolution)
+
+    return render_template('mosaic.html')
 
 
 @app.route('/')
 def entry_page():
     return redirect(url_for('mosaic'))
+
+
+def modify_images(pic_url_list, x_res, y_res):
+    """Function which gets images from urls and makes final mosaic image"""
+    pic_list = []
+
+    for pic in pic_url_list:
+        size = len(pic_url_list)
+
+        response = requests.get(pic)
+        img = (Image.open(BytesIO(response.content)))
+        img = img.resize((int(x_res / size), int(y_res / size)))
+        pic_list.append(img)
+
+    for pic in pic_list:
+        pic.show()
 
 
 if __name__ == '__main__':

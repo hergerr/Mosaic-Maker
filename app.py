e84ed209bf3090193f4735d61ee9aea63b8796c0
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-04-13 14:41:24 +0200

    Add mosaic for 1,2,3,4,6 and 8 images

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/mozaika')
-def mosaic():
+def make_mosaic():
     """This function deals with given url and returns template with mosaic"""
 
     # list with url for pictures
@@ -43,17 +43,40 @@ def entry_page():
 def modify_images(pic_url_list, x_res, y_res):
     """Function which gets images from urls and makes final mosaic image"""
     pic_list = []
+    counter = 0
+    size = len(pic_url_list)
 
-    for pic in pic_url_list:
-        size = len(pic_url_list)
+    mosaic = Image.new("RGB", (x_res, y_res), "white")
 
-        response = requests.get(pic)
+    if size == 1:
+        response = requests.get(pic_url_list[0])
         img = (Image.open(BytesIO(response.content)))
-        img = img.resize((int(x_res / size), int(y_res / size)))
-        pic_list.append(img)
+        img = img.resize((x_res, y_res))
+        mosaic.paste(img)
 
-    for pic in pic_list:
-        pic.show()
+    elif size == 2 or size == 3:
+        for pic in pic_url_list:
+            img_width = int(x_res / size)
+            img_height = int(y_res)
+
+            response = requests.get(pic)
+            img = (Image.open(BytesIO(response.content)))
+            img = img.resize((img_width, img_height))
+            mosaic.paste(img, (counter * img_width, 0))
+            counter += 1
+
+    elif size == 4 or size == 6 or size == 8:
+        for pic in pic_url_list:
+            img_width = int(x_res / (size / 2))
+            img_height = int(y_res / 2)
+
+            response = requests.get(pic)
+            img = (Image.open(BytesIO(response.content)))
+            img = img.resize((img_width, img_height))
+            mosaic.paste(img, ((counter % int(size/2))*img_width, int(counter/int(size/2))*img_height))
+            counter += 1
+
+    mosaic.show()
 
 
 if __name__ == '__main__':
